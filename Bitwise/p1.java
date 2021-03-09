a941e5eaec491b80859a825b3e847276497e284c
Nikhiljain195 <35103835+Nikhiljain195@users.noreply.github.com>
2021-03-09 22:28:44 +0530

    .

---
+++ Bitwise/p1.java
@@ -0,0 +1,13 @@
+public class p1 {
+    public static void main(String args[])
+    {
+        int a=5,b=6,x=3,y=-1;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(x<<2);
+        System.out.println(y<<1);
+    }
+    
+}
