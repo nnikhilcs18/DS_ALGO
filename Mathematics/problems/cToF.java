167c2ea8822f8879e0fd5166dde3da019ae1bb2a
Nikhiljain195 <35103835+Nikhiljain195@users.noreply.github.com>
2021-03-09 20:06:04 +0530

    .

---
+++ Mathematics/problems/cToF.java
@@ -0,0 +1,10 @@
+package problems;
+
+public class cToF {
+    public static void main(String args[])
+    {
+        double farenheit,celsius=50;
+        farenheit=((9.0/5)*celsius)+32;
+        System.out.println("farenheit="+ farenheit);
+    }
+}
