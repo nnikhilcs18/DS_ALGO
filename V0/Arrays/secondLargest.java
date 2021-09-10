9086c530d4ffa24e60d9de933ddc25d247faa9b1
NIKHIL N <nik@publicisgroupe.net>
2021-09-11 00:08:24 +0530

    old codes

---
+++ V0/Arrays/secondLargest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class secondLargest {
+    public static void main(String args[]){
+        int [] list={1,2,3,4,5,9,8,7,6,5,0};
+        int largest=list[0];
+        for(int i=1;i<list.length;i++)
+        {
+            if(list[i]>latgest)
+                largest=a[i];
+        }
+        second_largest(list,largest);
+    } 
+    static void second_largest(int []list, int largest)
+    {
+
+    }
+}
